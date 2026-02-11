53bd64f7e1623be04bf491c4239c77609425150f
AnGus400gr <augusdubois@gmail.com>
2026-02-11 16:26:24 +0100

    tp1.5

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
+import express from 'express';
+
 function greet(name: string): string {
     return `Hey ${name}!`;
 }
 console.log(greet("Augustin"))
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req , res ) => {
+    res.send('Bienvenue sur mon serveur API');
+});
+
+app.listen(port, () => {
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+});
